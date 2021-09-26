06b6141e8f3d8476734ff017a5fc3cbd6a0cefc4
Paweł Kęska <pawel.keska@WAWPC020085.pl.vml.lan>
2021-09-26 09:12:02 +0200

    initial commit.

---
+++ aspNetWseiLab3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace aspNetWseiLab3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
