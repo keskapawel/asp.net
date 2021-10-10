57f236257f3b906ea36466c01f1ac9840e7d723d
Paweł Kęska <pawel.keska@microsoft.wsei.edu.pl>
2021-10-10 09:28:30 +0200

    Flow from 10.10.2021.

---
+++ aspNetWseiLab3/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using aspNetWseiLab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetWseiLab3.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductModel product)
+        {
+            var viewModel = new ProductStatsViewModel
+            {
+                NameLength = product.Name.Length,
+                DescriptionLength = product.Description.Length,
+                NameValue = product.Name,
+                DescriptionValue = product.Description,
+            };
+            return View(viewModel);
+        }
+
+
+    }
+}
---
+++ aspNetWseiLab3/Models/ProductStatsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspNetWseiLab3.Models
+{
+    public class ProductStatsViewModel
+    {
+        public int DescriptionLength { get; set; }
+        public string DescriptionValue { get; set; }
+        public int NameLength { get; set; }
+        public string NameValue { get; set; }
+
+
+    }
+}
